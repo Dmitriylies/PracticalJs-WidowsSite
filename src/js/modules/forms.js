14dcada48bef8dc143a51511245a6df04ea03b8b
Dmitriy <dimalie@live.com>
2021-07-24 16:16:30 +0300

    Отправка формы.Регул. выр-ния. оповещение пользователя У.3

---
+++ src/js/modules/forms.js
@@ -0,0 +1,59 @@
+const forms = () => {
+    const form = document.querySelectorAll('form'),
+          input = document.querySelectorAll('input'),
+          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+//*6
+    phoneInputs.forEach(item => {
+        item.addEventListener('input', ()=> {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+//*2
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+//*4
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+        return await res.text();
+    };
+//*5
+    const clearInputs = () => {
+        input.forEach(item => {
+            item.value = '';
+        });
+    };
+//*3
+    form.forEach(item => {
+        item.addEventListener('submit', (e)=> {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+
+            postData('assets/server.php', formData)
+                .then(res => {
+                    console.log(res);
+                    statusMessage.textContent = message.success;
+                })
+                .catch(() => statusMessage.textContent = message.failure)
+                .finally(() => {
+                    clearInputs();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 5000);
+                });
+        });
+    });
+};
+
+export default forms;
